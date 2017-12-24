0a4e3ccddb36ed6ae792ebd24d91f90b04cded63
tsunglinken <magic3657@gmail.com>
2017-12-24 13:44:17 +0800

    modify for Heroku

--- app.py
+++ app.py
@@ -6,7 +6,9 @@
 # print(os.path.dirname(__name__))
 # sys.path.append(os.path.dirname(__name__))
 
+import os
 from toilet_business import create_app
 
 if __name__ == "__main__":
-    create_app().run()
+    port = int(os.environ.get('PORT', 5000))
+    create_app().run(host='0.0.0.0', port=port)
